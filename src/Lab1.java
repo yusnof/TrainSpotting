a2a3b5f99beb7758efc633d68f6b30494fa3a6b5
yusnof <yusnofal@gmail.com>
2024-09-10 14:38:46 +0200

    Run some tests and added the Point class

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,19 +1,30 @@
 import java.util.concurrent.Semaphore;
+import java.awt.Point; 
 
 import TSim.*;
 
 public class Lab1 {
-
-  Thread train1, train2; 
-  TSimInterface tsi; 
+  
+  final int  SWITCH_LEFT = 1; 
+  final int SWITCH_RIGHT = 0; 
+  final Thread train1, train2; 
+  final TSimInterface tsi; 
   Semaphore blue, red, green, pink, grey; 
   SensorEvent sensorEvent; 
+  // will calculating from top top down ToDo::declaring the hardcoded part of the Point. 
+  final Point switch1 = new Point(0, 0);
+  final Point switch2 = new Point(0, 0); 
+  final Point switch3 = new Point(0, 0);
+  final Point switch4 = new Point(0, 0);  
+  Point sensorPoint;   
+  
 
     // constructor for the lab1 where we will be .. 
     public Lab1(int speed1, int speed2) throws InterruptedException {
       tsi = TSimInterface.getInstance();
       blue = new Semaphore(1); 
-
+    
+      
       train1 = new Thread(() -> runTrain(1,speed1)); 
       train2 = new Thread(() -> runTrain(2,speed2)); 
 
@@ -27,9 +38,9 @@ public class Lab1 {
         tsi.setSpeed(trainId,speed);
         while (true) {
           sensorEvent = tsi.getSensor(trainId);
+          if(sensorEvent.getStatus() == sensorEvent.ACTIVE){
           handleSensorEvent(sensorEvent, trainId, speed);  
-        
-
+          };
         }
       }
       catch (CommandException e) {
@@ -38,11 +49,11 @@ public class Lab1 {
       }
       catch (InterruptedException e){
         e.printStackTrace();    // or only e.getMessage() for the error
-        System.exit(1);
-
+        System.exit(1); 
       }
     }
     private void handleSensorEvent(SensorEvent sensor, int trainId, int speed) throws CommandException, InterruptedException{
+      sensorPoint = new Point(sensor.getXpos(), sensor.getYpos()); 
       int x = sensor.getXpos(); 
       int y = sensor.getYpos();
       if (x == 5 && y == 11){
@@ -52,8 +63,22 @@ public class Lab1 {
     }
     private void handleTrainAtStation(int trainId, int speed) throws CommandException, InterruptedException{
       tsi.setSpeed(trainId, 0);
-      Thread.sleep(2000);
-      tsi.setSpeed(trainId, -speed);
+      // this solution is not very correct, because i am not sure what thread will sleep. 
+      if(trainId == 1){
+        train1.sleep(3000);
+      }
+      else if(trainId == 2){
+        train2.sleep(3000); 
+      }
+      else{
+        throw new InterruptedException ("not valid id");
+      }
+       tsi.setSpeed(trainId, -speed);
     }
-
+    private void handleSwitches(int xPos, int yPos, int switchDir ) throws CommandException, InterruptedException{
+      //introduce the logic behind the Semaphore and to switch 
+      tsi.setSwitch(0, 0, 0);
+    }
 }
+
+
