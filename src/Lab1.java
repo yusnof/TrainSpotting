eb5c41a875969d4ea68100db593801e2334af05e
yusnof <yusnofal@gmail.com>
2024-09-13 09:08:21 +0200

    yes1

--- src/Lab1.java
+++ src/Lab1.java
@@ -4,94 +4,131 @@ import java.awt.Point;
 import TSim.*;
 
 public class Lab1 {
-  TSimInterface tsi; 
+  
+  TSimInterface tsi = TSimInterface.getInstance();
+
+  final int trainId1 = 1; 
+  final int trainId2 = 2; 
+  final int MAX_SPEED = 20; 
+  int xPos; 
+  int yPos; 
+  Semaphore orange = new Semaphore(1); 
+  Semaphore red = new Semaphore(1); 
+
   
   
-  //final Thread train1; 
-  
-  Semaphore blue, red, green, pink, grey; 
+  // Semaphore blue, red, green, pink, grey; 
   SensorEvent sensorEvent; 
   // will calculating from top top down ToDo::declaring the hardcoded part of the Point. 
-  final Point switch1 = new Point(0, 0);
-  final Point switch2 = new Point(0, 0); 
-  final Point switch3 = new Point(0, 0);
-  final Point switch4 = new Point(0, 0);  
+  final Point switch1 = new Point(3, 11);
+  final Point switch2 = new Point(4, 9); 
+  final Point switch3 = new Point(15, 9);
+  final Point switch4 = new Point(17, 7);  
+  
   Point sensorPoint;   
-  
-  private class ThreadTrain implements Runnable{
-    int trainId; 
-    int speed; 
-    public ThreadTrain(int trainId, int speed){
-      this.trainId = trainId; 
-      this.speed = speed;
-    }
-    @Override
-    public void run() {
-      try {
-        tsi.setSpeed(trainId,speed);
-        while (true) {
-          sensorEvent = tsi.getSensor(trainId);
-          if(sensorEvent.getStatus() == sensorEvent.ACTIVE){
-          handleSensorEvent(sensorEvent, trainId, speed);  
-          };
-        }
-      }
-      catch (CommandException e) {
-        e.printStackTrace();    // or only e.getMessage() for the error
-        System.exit(1);
-      }
-      catch (InterruptedException e){
-        e.printStackTrace();    // or only e.getMessage() for the error
-        System.exit(1); 
-      }
-    }
-    }
-
-  
-
     // constructor for the lab1 where we will be .. 
-    public void Lab1(int speed1, int speed2) throws InterruptedException {
-      TSimInterface tsi = TSimInterface.getInstance();
-      blue = new Semaphore(1); 
+    public Lab1 (int speed1, int speed2) throws InterruptedException {
+    
+      // two object of our train thread. 
+      ThreadTrain train1 = new ThreadTrain(trainId1, speed1); 
+      ThreadTrain train2 = new ThreadTrain(trainId2, speed2); 
 
-      ThreadTrain train1 = new ThreadTrain(1, 10); 
-      ThreadTrain train2 = new ThreadTrain(2, 10); 
+      //starting of the thread which forces the thread to execute the run() method. 
+      train1.start();
+      train2.start();
+      
+      
     }
 
     // method that will be handling running of the train. 
     
     private void handleSensorEvent(SensorEvent sensor, int trainId, int speed) throws CommandException, InterruptedException{
-      sensorPoint = new Point(sensor.getXpos(), sensor.getYpos()); 
-      int x = sensor.getXpos(); 
-      int y = sensor.getYpos();
+      xPos = sensor.getXpos(); 
+      yPos = sensor.getYpos();
+      System.out.println("I was at sensorEven ");
+    
+      handleTrainAtStation(xPos, yPos, trainId, speed);
+      handleTrainSemaphore(xPos, yPos, trainId, speed);
       
-      if (x == 5 && y == 11){
-        handleTrainAtStation(trainId, speed);
-      } 
-
-
     }
-    private void handleTrainAtStation(int trainId, int speed) throws CommandException, InterruptedException{
+
+
+    private void handleTrainAtStation(int xPos, int yPos, int trainId, int speed) throws CommandException, InterruptedException{
+      if((xPos == 16 && yPos == 11) || (xPos == 16 && yPos == 13)){
+        System.out.println("I was at the station ");
       tsi.setSpeed(trainId, 0);
-      // this solution is not very correct, because i am not sure what thread will sleep. 
-      if(trainId == 1){
-        //train1.sleep(3000);
-        Thread.sleep(3000);
-      }
-      else if(trainId == 2){
-        // train2.sleep(3000); 
-        Thread.sleep(3000);
-      }
-      else{
-        throw new InterruptedException ("not valid id");
-      }
+       Thread.sleep(2000); 
        tsi.setSpeed(trainId, -speed);
+      }
     }
-    private void handleSwitches(int xPos, int yPos, int switchDir ) throws CommandException, InterruptedException{
-      //introduce the logic behind the Semaphore and to switch 
-      tsi.setSwitch(0, 0, 0);
+
+    private void handleTrainSemaphore(int xPos, int yPos, int trainId, int speed) throws CommandException, InterruptedException{
+
+      
+      if((xPos == 4 && yPos == 11) || (xPos == 4 && yPos == 13)){ //first and second signal
+          if(!orange.tryAcquire()){
+            tsi.setSpeed(trainId, 0);
+
+            orange.acquire();
+            tsi.setSpeed(trainId, speed);
+          }
+          if (yPos == 11){tsi.setSwitch(3, 11, tsi.SWITCH_LEFT);}
+          else{tsi.setSwitch(3, 11, tsi.SWITCH_RIGHT);}
+          orange.release();
+      }
+    
+    
+      }
+      
+    
+      class ThreadTrain extends Thread implements Runnable{
+        int trainId; 
+        int speed; 
+        dir direction; 
+  
+        public ThreadTrain(int trainId, int speed){
+          this.trainId = trainId; 
+          this.speed = speed;
+          // debatable
+          if(trainId == 1){
+            direction = dir.SOUTH;
+          };
+          if(trainId == 2){
+            direction = dir.NORTH;
+          };
+        }
+  
+        @Override
+        public void run() {
+          try {
+            tsi.setSpeed(trainId,speed);
+  
+            while (true) {
+              sensorEvent = tsi.getSensor(trainId);
+              if(sensorEvent.getStatus() == sensorEvent.ACTIVE){
+              handleSensorEvent(sensorEvent, trainId, speed); };
+            }
+          }
+          catch (CommandException e) {
+            e.printStackTrace();    // or only e.getMessage() for the error
+            System.exit(1);
+          }
+          catch (InterruptedException e){
+            e.printStackTrace();    // or only e.getMessage() for the error
+            System.exit(1); 
+          }
+        }
+        }
+        enum dir{
+          NORTH,SOUTH
+        }
+
     }
-}
+
+
+
+    
+
 
 
 
